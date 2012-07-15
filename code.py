4995d9928d250c70694fcc0ed8a4fac3c4210951
Natsuki Mori <morinatsu@gmail.com>
2012-07-15 17:22:40 +0900

    make isHot()

--- code.py
+++ code.py
@@ -4,24 +4,74 @@
 toodledo2grwol : Display grwol notify tasks of Toodledo
 
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import argparse
 import ConfigParser
 from poodledo.apiclient import ApiClient
 import gntp.notifier
 
-# Task Filters
-def _taskFilter(tasks):
+
+# Hotlist Filter
+def _HotlistFilter(account_info, tasks):
     """
-     Task Filter -- return filtered tasks
+     Hotlist Filter -- return tasks in Hotlist
     """
+
+    def _isHot(task):
+        """
+        If a task in Hostlist returns True
+        """
+
+        # priority
+        if not hasattr(account_info, "hotlistpriority"):
+            logging.debug('hotlistpriority is not setted.')
+        else:
+            if task.priority < account_info.hotlistpriority:
+                logging.debug(': '.join(['less priority', task.title,
+                    str(task.priority)]))
+                return False
+
+        # duedate
+        if not hasattr(account_info, "hotlistduedate"):
+            logging.debug('hotlistduedate is not setted.')
+        else:
+            today = datetime.today()
+            from_date = datetime.fromtimestamp(float(task.duedate)) - \
+                timedelta(account_info.hotlistduedate)
+            if today < from_date:
+                logging.debug(': '.join(['duedate is far', task.title,
+                    str(task.duedate)]))
+                return False
+
+        # star
+        if not hasattr(account_info, "hotliststar"):
+            logging.debug('hotliststar is not setted.')
+        else:
+            if account_info.hotliststar == '1':
+                if task.star != '1':
+                    logging.debug(': '.join(['not starred', tasl.title,
+                        task.star, account_info.hotliststar]))
+                    return False
+
+        # status
+        if not hasattr(account_info, "hotliststatus"):
+            logging.debug('hotliststatus is not setted.')
+        else:
+            if account_info.hotliststatus == '1':
+                if task.status != '1':
+                    logging.debug(': '.join(['is not Next Action', task.title,
+                        task.status, account_info.hotliststatus]))
+                    return False
+
+        logging.debug(' '.join([task.title, 'is in Hotlist.']))
+        return True
 
     def _isUncompleted(task):
         """
-         Task Uncompleted 
+         Task Uncompleted
         """
-        if hasattr(task, "completed") == False:
+        if not hasattr(task, "completed"):
             logging.debug(': '.join(['not completed', task.title]))
             return True
         else:
@@ -34,23 +84,7 @@ def _taskFilter(tasks):
                     str(task.completed)]))
                 return False
 
-    def _hasDuedate(task):
-        """
-         Task Has Duedate
-        """
-        if hasattr(task, "duedate") == True:
-            if task.duedate != 0:
-                logging.debug(': '.join(['has duedate', task.title]))
-                return True
-            else:
-                logging.debug(': '.join(['has not duedate', task.title]))
-                return False
-        else:
-            logging.debug(': '.join(['has duedate', task.title]))
-            return False
-
-    for task in [ task for task in tasks if (_isUncompleted(task) == True) and \
-                                            (_hasDuedate(task) == True)]:
+    for task in [task for task in tasks if (_isHot(task) and _isUncompleted(task))]:
         yield task
 
 #parse arguments
@@ -87,32 +121,35 @@ logging.debug(': '.join(['Auth password', password]))
 api.authenticate(email, password)
 logging.info('Auth Toodledo')
 
+# Get AccountInfo
+account_info = api.getAccountInfo()
+logging.debug(': '.join(['AccountInfo', str(account_info)]))
+
 # Get Task list from Toodledo
-task_list = api.getTasks(fields="duedate")
+task_list = api.getTasks(fields="duedate,star,priority")
 logging.info(': '.join(['Got task list', str(len(task_list))]))
 
 # Register to Growl
 growl = gntp.notifier.GrowlNotifier(
-    applicationName = "toodledo2growl",
-    notifications = ["Task"],
-    defaultNotifications = ["Task"],
+    applicationName="toodledo2growl",
+    notifications=["Task"],
+    defaultNotifications=["Task"],
 )
 growl.register()
 logging.info('Register growl')
 
 # Send Notify to Growl
 notify_icon = config.get('icon', 'notify')
-for hot_task in _taskFilter(task_list):
+for hot_task in _HotlistFilter(account_info, task_list):
     duedate = datetime.fromtimestamp(float(hot_task.duedate))
     growl.notify(
-        noteType = "Task",
-        title = hot_task.title,
-        description = duedate.strftime("DueDate: %Y/%m/%d"),
-        icon = open(notify_icon, 'rb').read(),
-        sticky = False,
-        priority = 1,
+        noteType="Task",
+        title=hot_task.title,
+        description=duedate.strftime("DueDate: %Y/%m/%d"),
+        icon=open(notify_icon, 'rb').read(),
+        sticky=False,
+        priority=1,
     )
     logging.info(': '.join(['notify sended', hot_task.title]))
 # end
 logging.info('end')
-
